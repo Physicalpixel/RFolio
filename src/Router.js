ea72a103581b186111104a96595b7e0a4a0f90d5
Physicalpixel <preethi.karan91@gmail.com>
2023-01-10 22:55:56 -0500

    changed to hasrouter

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,17 @@
 import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HashRouter, Route, Routes } from "react-router-dom"
 import App from "./App"
 import Home from "./Home"
 import Datagen from "./Datagen"
 
 export default function Router() {
 	return (
-		<BrowserRouter basename="./Rfolio">
+		<HashRouter>
 			<Routes>
 				<Route path="/" element={<Home />}></Route>
 				<Route path="/App" element={<App />}></Route>
 				<Route path="/Datagen" element={<Datagen />}></Route>
 			</Routes>
-		</BrowserRouter>
+		</HashRouter>
 	)
 }
